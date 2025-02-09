399e4ab07d885530bf73296dee8de6d6585ecdb0
Yuri Souza <ssouzayuri@gmail.com>
2025-02-09 11:37:55 -0300

    Update GitHubAPI.java

--- demo-backend/demo-backend/src/main/java/kontulari/kontchallege/demo_backend/GitHub/GitHubAPI.java
+++ demo-backend/demo-backend/src/main/java/kontulari/kontchallege/demo_backend/GitHub/GitHubAPI.java
@@ -2,13 +2,10 @@ package kontulari.kontchallege.demo_backend.GitHub;
 
 import kontulari.kontchallege.demo_backend.GitHub.dto.GitHubResultDTO;
 import kontulari.kontchallege.demo_backend.GitHub.dto.RepositoryDTO;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -20,22 +17,28 @@ public class GitHubAPI {
     private static final String BASE_URL = "https://api.github.com/users/";
     private final RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${GITHUB_TOKEN}")
+    private String githubToken;
+
+    private HttpHeaders createHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + githubToken);
+        return headers;
+    }
+
     @GetMapping("{user}")
     public GitHubResultDTO consultaGitHub(@PathVariable String user) {
         String url = BASE_URL + user;
-        ResponseEntity<GitHubResultDTO> response = restTemplate.getForEntity(url, GitHubResultDTO.class);
+        HttpEntity<String> entity = new HttpEntity<>(createHeaders());
+        ResponseEntity<GitHubResultDTO> response = restTemplate.exchange(url, HttpMethod.GET, entity, GitHubResultDTO.class);
         return response.getBody();
     }
 
     @GetMapping("{user}/repos")
     public List<RepositoryDTO> consultaRepos(@PathVariable String user) {
         String url = BASE_URL + user + "/repos";
-        ResponseEntity<List<RepositoryDTO>> response = restTemplate.exchange(
-                url,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<>() {}
-        );
+        HttpEntity<String> entity = new HttpEntity<>(createHeaders());
+        ResponseEntity<List<RepositoryDTO>> response = restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {});
         return response.getBody();
     }
 }

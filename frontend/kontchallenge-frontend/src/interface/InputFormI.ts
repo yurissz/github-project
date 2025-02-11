7febc3e362b857ba9600ca048675eb962d0b1849
Yuri Souza <ssouzayuri@gmail.com>
2025-02-11 13:30:50 -0300

    Delete frontend directory

--- frontend/kontchallenge-frontend/src/interface/InputFormI.ts
+++
@@ -1,9 +0,0 @@
-import { RepositoryI } from "./RepositoryI";
-import { UsernameI } from "./UsernameI";
-
-export interface InputFormProps {
-    username: UsernameI | undefined;
-    setUsername: (newUsername: UsernameI | undefined) => void;
-    repos: RepositoryI[] | undefined;
-    setRepos: (newRepos: RepositoryI[] | undefined) => void;
-}
--- frontend/kontchallenge-frontend/src/utils/formatToDate.ts
+++
@@ -1,9 +0,0 @@
-import { format } from "date-fns";
-
-export const formatDate = (data: string) => {
-    if (typeof data !== 'string' || !data) {
-        return 'Data inválida';
-    }
-    const dataAtual = new Date(data);
-    return format(dataAtual, 'dd/MM/yyyy');
-}
